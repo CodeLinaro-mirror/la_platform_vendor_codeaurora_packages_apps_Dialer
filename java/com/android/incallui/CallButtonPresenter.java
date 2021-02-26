2ceef29ef0f80ab6e46cba30ab312f29d29eb332
Sonalika Paul <psonalik@codeaurora.org>
2021-02-26 14:13:39 +0530

    IMS: Fix static image feature related issues.
    - Fix hide me option shown when incoming VT
    call is received on other sub which is in
    ROW mode. Update map as per carrier config
    instead of returning if config is false.
    
    - Fix camera remains closed and switch camera
    remains greyed out when incoming VT call is
    received on other sub and hide me is clicked
    during call in first sub.
    
    Change-Id: I59ba6a44e04f703edee95e2f25b33f377d7d363a
    CRs-Fixed: 2881495

--- java/com/android/incallui/CallButtonPresenter.java
+++ java/com/android/incallui/CallButtonPresenter.java
@@ -576,7 +576,7 @@ public class CallButtonPresenter
     inCallButtonUi.showButton(
         InCallButtonIds.BUTTON_SWITCH_CAMERA,
         isVideo && hasCameraPermission && call.getVideoTech().isTransmitting()
-        && !BottomSheetHelper.getInstance().isHideMeSelected()
+        && !BottomSheetHelper.getInstance().isInHideMeMode()
         && !ScreenShareHelper.screenShareRequested());
     inCallButtonUi.showButton(InCallButtonIds.BUTTON_PAUSE_VIDEO, showPauseVideo);
     if (isVideo) {
@@ -620,11 +620,9 @@ public class CallButtonPresenter
    */
   @Override
   public void onSendStaticImageStateChanged(boolean shallTransmitStaticImage) {
-    if (call == null || !QtiImsExtUtils.shallShowStaticImageUi(
-         BottomSheetHelper.getInstance().getPhoneId(), context)) {
+    if (call == null) {
        return;
      }
-
      updateButtonsState(call);
   }
 
