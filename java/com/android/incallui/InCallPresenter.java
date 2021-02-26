29c5396ae1a15d2973cd1983024924a53e7834cf
Pengfei Xiao <pengfeix@codeaurora.org>
2021-02-26 11:24:48 +0800

    IMS-VT: Update video call state in notification
    - Update video call state in notification properly when
    VT call is switched between forground and background.
    
    Change-Id: I0f8d60e1aa224ab14c2e6db198477ee062f353dc
    CRs-Fixed: 2886945

--- java/com/android/incallui/InCallPresenter.java
+++ java/com/android/incallui/InCallPresenter.java
@@ -1268,6 +1268,10 @@ public class InCallPresenter implements CallList.Listener, AudioModeProvider.Aud
       proximitySensor.onInCallShowing(showing);
     }
 
+    if (statusBarNotifier != null) {
+      statusBarNotifier.updateNotification();
+    }
+
     if (showing) {
       refreshMuteState();
     } else {
