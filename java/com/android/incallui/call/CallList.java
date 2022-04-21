70e47f6f379d1af29da334ba9811376d44d0fb82
Muhammed Siju <quic_c_msiju@quicinc.com>
2022-04-21 22:06:30 +0530

    Fix issue of call end dialog not getting displayed sometimes.
    When MO call is ended immediately after dialing, sometimes
    call is getting added to Dialer in disconnected state.
    Currently, dialer doesn't track calls added directly in
    disconnected state. So the call fail cause dialog is not
    displayed in such cases.
    Fix this by allowing calls to be tracked even when
    they are directly getting added in disconnected state.
    Make sure the disconnected call is added only once to
    the call list.
    
    Change-Id: I48d793a7c4710b5a7d06af2bcc9b430e21be6ae2
    CRs-Fixed: 3151268

--- java/com/android/incallui/call/CallList.java
+++ java/com/android/incallui/call/CallList.java
@@ -726,8 +726,8 @@ public class CallList implements DialerCallDelegate {
     boolean updated = false;
 
     if (call.getState() == DialerCallState.DISCONNECTED) {
-      // update existing (but do not add!!) disconnected calls
-      if (callById.containsKey(call.getId())) {
+      // Make sure disconnected calls are added only once
+      if (callById.containsKey(call.getId()) || !call.wasCallAddedToCallList()) {
         // For disconnected calls, we want to keep them alive for a few seconds so that the
         // UI has a chance to display anything it needs when a call is disconnected.
 
@@ -739,11 +739,13 @@ public class CallList implements DialerCallDelegate {
         callById.put(call.getId(), call);
         callByTelecomCall.put(call.getTelecomCall(), call);
         updated = true;
+        call.markCallAddedToCallList();
       }
     } else if (!isCallDead(call)) {
       callById.put(call.getId(), call);
       callByTelecomCall.put(call.getTelecomCall(), call);
       updated = true;
+      call.markCallAddedToCallList();
     } else if (callById.containsKey(call.getId())) {
       callById.remove(call.getId());
       callByTelecomCall.remove(call.getTelecomCall());
