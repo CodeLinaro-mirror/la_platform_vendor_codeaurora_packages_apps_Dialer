364325d467bbf6741ca6a1cc2f55ac69e3de9ca1
Wileen Chiu <quic_wileenc@quicinc.com>
2022-11-16 17:58:07 -0800

    DSDS Transition: Notify before disconnecting calls
    - When the DSDA -> DSDS Transition occurs, there
      may be a case where there are concurrent calls.
    - If the user attempts to place another call, then
      calls on the other SUB will be disconnected
    - Notify the user and place the call if the user
      agrees to letting the other calls disconnect
    - Introduce a new TextView to show warning message
      when additional PhoneAccount selection is needed
    
    Change-Id: If5368e9f6df41330640aa57e1b6abfe9d57826e7
    CRs-Fixed: 3435872

--- java/com/android/dialer/activecalls/ActiveCallInfo.java
+++ java/com/android/dialer/activecalls/ActiveCallInfo.java
@@ -28,6 +28,7 @@ public abstract class ActiveCallInfo {
 
   /** The {@link PhoneAccountHandle} the call is made with */
   public abstract Optional<PhoneAccountHandle> phoneAccountHandle();
+  public abstract int state();
 
   public static Builder builder() {
     return new AutoValue_ActiveCallInfo.Builder();
@@ -42,7 +43,7 @@ public abstract class ActiveCallInfo {
     }
 
     public abstract Builder setPhoneAccountHandle(Optional<PhoneAccountHandle> phoneAccountHandle);
-
+    public abstract Builder setState(int state);
     public abstract ActiveCallInfo build();
   }
 }
