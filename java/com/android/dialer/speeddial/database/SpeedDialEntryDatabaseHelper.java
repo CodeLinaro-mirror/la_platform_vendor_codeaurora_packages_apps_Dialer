AOSP-Platform/Dialer

Use Option.ofNullable instead of Optional.of
Optional.of expects a non-null value, and throws a
NullPointerException if it gets null.

This needs to be fixed to upgrade errorprone to v3.8.1.

Bug: 190944875
Test: Presubmits
Change-Id: I38b5d80ec8c1e2dc93d76e08a0d0051fc4127ab3

java/com/android/dialer/speeddial/database/SpeedDialEntryDatabaseHelper.java
@@ -125,7 +125,7 @@ public final class SpeedDialEntryDatabaseHelper extends SQLiteOpenHelper
               Channel.builder()
                   .setNumber(number)
                   .setPhoneType(cursor.getInt(POSITION_PHONE_TYPE))
-                  .setLabel(Optional.of(cursor.getString(POSITION_PHONE_LABEL)).or(""))
+                  .setLabel(Optional.ofNullable(cursor.getString(POSITION_PHONE_LABEL)).or(""))
                   .setTechnology(cursor.getInt(POSITION_PHONE_TECHNOLOGY))
                   .build();
         }

CodeLinaro-mirror/la_platform_vendor_codeaurora_packages_apps_Dialer

NullPointerException if it gets null
(cherry picked from commit 74b1dd97d839fff2f70bdade2b82c4448a22e964)
CRs-Fixed: 3361397

java/com/android/dialer/speeddial/database/SpeedDialEntryDatabaseHelper.java
@@ -125,7 +125,7 @@ public final class SpeedDialEntryDatabaseHelper extends SQLiteOpenHelper
               Channel.builder()
                   .setNumber(number)
                   .setPhoneType(cursor.getInt(POSITION_PHONE_TYPE))
-                  .setLabel(Optional.of(cursor.getString(POSITION_PHONE_LABEL)).or(""))
+                  .setLabel(Optional.fromNullable(cursor.getString(POSITION_PHONE_LABEL)).or(""))
                   .setTechnology(cursor.getInt(POSITION_PHONE_TECHNOLOGY))
                   .build();
         }