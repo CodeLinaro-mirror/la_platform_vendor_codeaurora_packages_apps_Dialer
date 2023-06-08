f91d080b430e3b8167854b6ebde9c0fb188c5223
Zhao Fan <quic_zhaof@quicinc.com>
2023-06-08 17:52:02 +0530

    Update button state when multi window mode changed
    Change-Id: I59e97502d1ce2c5ed307b7bab24b21b149f1135b
    CRs-Fixed: 3445111

--- java/com/android/incallui/incall/impl/InCallFragment.java
+++ java/com/android/incallui/incall/impl/InCallFragment.java
@@ -656,6 +656,7 @@ public class InCallFragment extends Fragment
         // In case of activity relaunch, the onViewCreate isn't yet called.
         contactGridManager.onMultiWindowModeChanged(isInMultiWindowMode);
     }
+    updateButtonStates();
   }
 
   private Fragment getLocationFragment() {
