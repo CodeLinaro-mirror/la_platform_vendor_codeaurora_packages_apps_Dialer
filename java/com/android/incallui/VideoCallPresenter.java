b9e1cd9a01175cf9251cf5e2a317597e1be7b1cd
Lakshminath Alamuru <quic_lalamuru@quicinc.com>
2023-06-28 17:04:27 +0530

    Open Camera for MT VT Preview after application foreground
    - Unable to open Camera for MT VT Preview,
      when IncallActivity is in the background
    - Once Application becomes foreground, enable camera
    - OnResume of InCallActivity, with UIlistener open
      the camera
    
    Change-Id: I7ae0654d8a6368f3c9aabfbb25692ef8230e2ac2
    CRs-Fixed: 3526742

--- java/com/android/incallui/VideoCallPresenter.java
+++ java/com/android/incallui/VideoCallPresenter.java
@@ -224,6 +224,21 @@ public class VideoCallPresenter
      }
   };
 
+  /*UiListener instance to get MT VT preview on CallResume in Screen off mode */
+  private CallList.UiListener mUiListener = new CallList.UiListener() {
+    @Override
+    public void onCallAdded() {
+      //No-op
+    }
+
+    @Override
+    public void onInCallUiShown() {
+      LogUtil.d("VideoCallPresenter.onInCallUiShown",
+         "triggering enableCamera after onresume InCallActivity");
+      enableCamera(primaryCall, isCameraRequired());
+    }
+  };
+
   private void maybeCreateQtiImsExtConnector(Context context) {
     try {
       mQtiImsExtConnector = new QtiImsExtConnector(context,
@@ -500,6 +515,8 @@ public class VideoCallPresenter
     InCallPresenter.getInstance().getLocalVideoSurfaceTexture().setDelegate(new LocalDelegate());
     InCallPresenter.getInstance().getRemoteVideoSurfaceTexture().setDelegate(new RemoteDelegate());
 
+    CallList.getInstance().setUiListener(mUiListener);
+
     // Register for surface and video events from {@link InCallVideoCallListener}s.
     InCallVideoCallCallbackNotifier.getInstance().addSurfaceChangeListener(this);
     mPictureModeHelper.setUp(this);
@@ -539,6 +556,8 @@ public class VideoCallPresenter
     InCallPresenter.getInstance().removeOrientationListener(this);
     InCallPresenter.getInstance().removeInCallEventListener(this);
     InCallPresenter.getInstance().getLocalVideoSurfaceTexture().setDelegate(null);
+
+    CallList.getInstance().setUiListener(null);
 
     InCallVideoCallCallbackNotifier.getInstance().removeSurfaceChangeListener(this);
     InCallVideoCallCallbackNotifier.getInstance().removeVideoEventListener(this);
