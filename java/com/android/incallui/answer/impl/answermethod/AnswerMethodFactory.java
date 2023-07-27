6d4b66fb4d4e777f486f29a128da10fb18cf1348
hej <quic_hej@quicinc.com>
2023-07-27 21:17:00 +0800

    Dialer:Fix can't hang up the call by slide down the icon
    Change-Id: Ib4b3fc2ae686abf2fd98ffb19f6ccb1d0b621c0a

--- java/com/android/incallui/answer/impl/answermethod/AnswerMethodFactory.java
+++ java/com/android/incallui/answer/impl/answermethod/AnswerMethodFactory.java
@@ -12,6 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License
+ *
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 package com.android.incallui.answer.impl.answermethod;
@@ -58,6 +63,7 @@ public class AnswerMethodFactory {
       return true;
     }
 
-    return AccessibilityUtil.isTouchExplorationEnabled(activity) || activity.isInMultiWindowMode();
+    return AccessibilityUtil.isTouchExplorationEnabled(activity) || activity.isInMultiWindowMode()
+        || activity.isInMultiWindowMode() || activity.getResources().getBoolean(R.bool.need_show_two_button);
   }
 }
