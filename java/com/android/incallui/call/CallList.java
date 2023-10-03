09055d6bf9ba6c2cc7c68b770f3b4489442d5cc8
Akshay Sundaram <quic_akshsund@quicinc.com>
2023-10-03 12:13:15 -0700

    DSDA: Fix swap button disabled issue
    - CallList previous behavior did not update the last held call correctly because there were no active calls due to remote disconnection. This caused UI buttons to be disabled.
    - New behavior is to push held calls to a queue and remove calls when they move out of the hold state. Last held call is updated by looking at top of queue.
    
    Change-Id: I5e16f04bb20346dbeffc96693fc8f0b04565d442
    CRs-Fixed: 3614183

--- java/com/android/incallui/call/CallList.java
+++ java/com/android/incallui/call/CallList.java
@@ -78,9 +78,8 @@ public class CallList implements DialerCallDelegate {
   private final Map<String, DialerCall> callById = new ArrayMap<>();
   private final Map<android.telecom.Call, DialerCall> callByTelecomCall = new ArrayMap<>();
   private DialerCall secondaryCall;
-  private DialerCall lastActiveCall;
-  private DialerCall lastHeldCall;
   private String selectedIncomingCall;
+  private ArrayList<DialerCall> heldCallList = new ArrayList<>();
 
   /**
    * ConcurrentHashMap constructor params: 8 is initial table size, 0.9f is load factor before
@@ -544,8 +543,24 @@ public class CallList implements DialerCallDelegate {
   }
 
   public DialerCall getLastHeldCall() {
-    return (lastHeldCall != null && !isCallDead(lastHeldCall) &&
-        lastHeldCall.getState() != DialerCallState.DISCONNECTED) ? lastHeldCall : null;
+    for (int i = heldCallList.size() - 1; i >= 0; i--) {
+        DialerCall call = heldCallList.get(i);
+        if (call != null && !isCallDead(call) && call.getState() != DialerCallState.DISCONNECTED) {
+            return call;
+        }
+    }
+    return null; 
+  }
+
+  // Add calls that move into the held state to the end of the list. Remove calls that move out
+  // of the held state from the list.
+  private void updateLastHeldCall(DialerCall call) {
+     if (call.getState() == DialerCallState.ONHOLD && !heldCallList.contains(call)) {
+       heldCallList.add(call);
+     } else if (call.getState() != DialerCallState.ONHOLD && heldCallList.contains(call)) {
+       heldCallList.remove(call);
+     }
+    LogUtil.d("heldcallist", String.valueOf(heldCallList));
   }
 
   public void setSelectedIncomingCall(String id) {
@@ -688,15 +703,10 @@ public class CallList implements DialerCallDelegate {
       // calls doesn't contain the call which received the update.
       return;
     }
-
+    updateLastHeldCall(call);
     if (updateCallInMap(call)) {
       LogUtil.i("CallList.onUpdateCall", String.valueOf(call));
     }
-    if (call == lastActiveCall && call.getState() == DialerCallState.ONHOLD) {
-      lastHeldCall = call;
-    }
-    DialerCall activeCall = getActiveCall();
-    lastActiveCall = activeCall == null ? lastActiveCall: activeCall;
     if (Objects.equals(call.getId(), selectedIncomingCall) &&
         (call.getState() != DialerCallState.INCOMING ||
         call.getState() != DialerCallState.CALL_WAITING)) {
