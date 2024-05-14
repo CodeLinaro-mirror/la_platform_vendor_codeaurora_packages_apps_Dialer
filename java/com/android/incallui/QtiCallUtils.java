a73edb304b51d303475eed096e87d251c7af84a3
Yupeng Hou <quic_yupehou@quicinc.com>
2024-05-14 13:37:47 +0530

    Add unique display names for SIM selection in conference dialer
    Change-Id: I6e6c47a03b0a08a94f2441307dcca601c570c6e7
    CRs-Fixed: 3808264

--- java/com/android/incallui/QtiCallUtils.java
+++ java/com/android/incallui/QtiCallUtils.java
@@ -446,8 +446,23 @@ public class QtiCallUtils {
             SubscriptionInfo info = subscriptionManager.
                     getActiveSubscriptionInfoForSimSlotIndex(slotId);
             if (info != null) {
-                subInfoList.add(info.getDisplayName().toString());
-                subIdList.add(info.getSubscriptionId());
+                String phoneNumber = "";
+                int subId = info.getSubscriptionId();
+                try {
+                    phoneNumber = subscriptionManager.getPhoneNumber(subId);
+                } catch (IllegalStateException
+                        | SecurityException
+                        | UnsupportedOperationException e) {
+                    Log.w(LOG_TAG, "get number error." + e);
+                }
+                // If we could get the phone number, shows operator name + phone number.
+                // Otherwise, shows operator name + slotId
+                if (phoneNumber != null && !phoneNumber.isEmpty()) {
+                    subInfoList.add(info.getDisplayName().toString() + "\n" + phoneNumber);
+                } else {
+                    subInfoList.add(info.getDisplayName().toString() + " " + slotId);
+                }
+                subIdList.add(subId);
             }
         }
 
