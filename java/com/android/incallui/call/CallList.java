6789b9b219bf00aa75385f515e009c2b44f1ccdf
Suresh Koleti <quic_skoleti@quicinc.com>
2024-12-14 19:21:48 +0530

    Revert "DSDA: Check for last HELD call"
    This reverts commit 5beba4dae6bdfd7fb095950f378b9105d940d4a6.
    
    Change-Id: Ica7fdaafdb450cc57ffc21f0afa9d3f907f39a2f

--- java/com/android/incallui/call/CallList.java
+++ java/com/android/incallui/call/CallList.java
@@ -481,9 +481,7 @@ public class CallList implements DialerCallDelegate {
   public DialerCall getActiveOrBackgroundCall() {
     DialerCall call = getActiveCall();
     if (call == null) {
-      //If we have multiple held calls, and no active call, the call in
-      //foreground will be the last call which went into held state.
-      call = getBackgroundCalls().size() > 1 ? getLastHeldCall() : getBackgroundCall();
+      call = getBackgroundCall();
     }
     return call;
   }
