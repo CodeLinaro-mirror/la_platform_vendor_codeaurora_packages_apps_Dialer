f692dd85ec97014695cfb9f9256aa3930eb6b336
Suresh Koleti <quic_skoleti@quicinc.com>
2025-01-07 16:17:10 +0530

    Revert "IMS : Fix HUN when we press home button fron InCallScreen with multiple calls"
    This reverts commit a5af953358f784ffe2c432f317195d8411c83dfe.
    
    Change-Id: I516f4da6ae9b347360f3887c99b1548333b47ece

--- java/com/android/incallui/StatusBarNotifier.java
+++ java/com/android/incallui/StatusBarNotifier.java
@@ -146,7 +146,6 @@ public class StatusBarNotifier
   private String savedContent = null;
   private Bitmap savedLargeIcon;
   private String savedContentTitle;
-  private boolean wasShowingInCallUi = false;
   private CallAudioState savedCallAudioState;
   private int savedIncomingCallCount;
   private Uri ringtone;
@@ -370,7 +369,7 @@ public class StatusBarNotifier
         // however for case 2, ststus bar notifier is already aware of both incoming calls,
         // hence second check is needed to show the proper notification type
         notificationType = isShowingInCallUi &&
-            (callList.getIncomingCalls().size() == savedIncomingCallCount)
+            !hasMultipleIncomingCalls()
                 ? NOTIFICATION_INCOMING_CALL_QUIET
                 : NOTIFICATION_INCOMING_CALL;
       } else {
@@ -639,9 +638,6 @@ public class StatusBarNotifier
     } else {
       largeIconChanged = largeIcon == null || !savedLargeIcon.sameAs(largeIcon);
     }
-    // HUN is needed for the use case when we move out of InCallUi to HomeScreen
-    boolean isShowingInCallUi = InCallPresenter.getInstance().isShowingInCallUi();
-    boolean shouldUpdateNotificationForInCallUi = wasShowingInCallUi && !isShowingInCallUi;
 
     // any change means we are definitely updating
     boolean retval =
@@ -653,8 +649,7 @@ public class StatusBarNotifier
             || contentTitleChanged
             || !Objects.equals(this.ringtone, ringtone)
             || !Objects.equals(savedCallAudioState, callAudioState)
-            || (numOfIncomingCalls != savedIncomingCallCount)
-            || shouldUpdateNotificationForInCallUi;
+            || (numOfIncomingCalls != savedIncomingCallCount);
 
     LogUtil.d(
         "StatusBarNotifier.checkForChangeAndSaveData",
@@ -689,7 +684,6 @@ public class StatusBarNotifier
     this.ringtone = ringtone;
     savedCallAudioState = callAudioState;
     savedIncomingCallCount = numOfIncomingCalls;
-    wasShowingInCallUi = isShowingInCallUi;
 
     if (retval) {
       LogUtil.d(
