a6968da8d9f5aa85f2f8b5618702d2c4a1bfac3a
Akshay Sundaram <quic_akshsund@quicinc.com>
2025-06-17 09:52:17 -0700

    Dialer: Upgrade VoLte to Screen Share
    - Issue was that when attempting to upgrade from VoLte call to
      screen sharing, call would instead simply upgrade to VT call
      but not screen sharing. This was due to a condition that checks
      if screen sharing is requested in onPrimaryCallChanged and if so,
      to exit the screen sharing.
    - Fix is to adjust that condition to check if the previous primary
      call was null. This is only the case when upgrading from VoLte to
      VT call since VideoCallPresenter does not have a primary call yet.
      In this case, we want to enter screen sharing so the condition is
      passed through when upgrading VoLTE to VT.
    
    Change-Id: I3bdb0a1d8bab0266f307c306abb076063e0d064f
    CRs-Fixed: 4181754

--- java/com/android/incallui/VideoCallPresenter.java
+++ java/com/android/incallui/VideoCallPresenter.java
@@ -1157,7 +1157,7 @@ public class VideoCallPresenter
   private void onPrimaryCallChanged(DialerCall newPrimaryCall) {
     final boolean shouldShowVideoUi = shouldShowVideoUiForCall(newPrimaryCall);
     final boolean isVideoMode = isVideoMode();
-    if (ScreenShareHelper.screenShareRequested()) {
+    if (ScreenShareHelper.screenShareRequested() && primaryCall != null) {
         exitScreenShare();
         clearScreenShareStates();
     }
