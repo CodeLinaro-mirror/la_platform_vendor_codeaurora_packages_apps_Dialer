4a2a61537efab3581081d6f800addc19d5bcb944
Yupeng Hou <quic_yupehou@quicinc.com>
2025-06-17 07:56:04 +0530

    Fix no end button shown when dialing CRBT VT call is ended remotely
    - It is timing issue. When VT call with video CRBT is ended remotely
    in dialing/alerting state, there exists code running time that
    receives rx_pause but videocall fragment doesn't finish entering
    green screen mode, that means previewTextureView which is camera
    preview does not override remoteTextureView which is network video
    stream. During this period of time, if user clicks the screen, the
    click event will be consumed by RemoteDelegate, but incoming video
    stream from network is ended, so this click will be handled and enter
    the full screen mode. Due to this call is in dialing/alerting state,
    enter/exit full screen mode is not allowed, UI will stay in full
    screen mode and no end button for user to click to end this
    call.
    
    - Ignore click event for all cases which show remote video during
    dialing state.
    
    Change-Id: Ife174a8ff6ab88a35946f1600413c2d9bd5bb782
    CRs-Fixed: 4174452

--- java/com/android/incallui/VideoCallPresenter.java
+++ java/com/android/incallui/VideoCallPresenter.java
@@ -2141,7 +2141,7 @@ public class VideoCallPresenter
     @Override
     public void onSurfaceClick(VideoSurfaceTexture videoCallSurface) {
       // Show zoom control when preview surface is clicked.
-      LogUtil.i("VideoCallPresenter.onSurfaceClick", "");
+      LogUtil.i("VideoCallPresenter.onSurfaceClick", "LocalDelegate");
       if (shallTransmitStaticImage()) {
         VideoCallPresenter.this.onSurfaceClick();
       } else if (mPictureModeHelper != null && mPictureModeHelper.canShowPreviewVideoView()
@@ -2185,14 +2185,11 @@ public class VideoCallPresenter
 
     @Override
     public void onSurfaceClick(VideoSurfaceTexture videoCallSurface) {
-      boolean isCrbtReady = isIncomingVideoAvailableForEarlyMedia();
-      // Set CRBT call and visualized voice call not support full screen mode.
-      if ((QtiCallUtils.hasVideoCrbtVtCall(context) && isCrbtReady)
-          || QtiCallUtils.hasVideoCrbtVoLteCall(context)
-          || QtiCallUtils.isVisualizedVoiceCall()) {
-        LogUtil.i(
-            "VideoCallPresenter.RemoteDelegate",
-            "ignore to enter full screen mode for CRBT/UVS call.");
+      LogUtil.i("VideoCallPresenter.onSurfaceClick", "RemoteDelegate");
+      if (primaryCall != null
+          && (primaryCall.getState() == DialerCallState.DIALING
+          || primaryCall.getState() == DialerCallState.CONNECTING)) {
+        LogUtil.i("VideoCallPresenter.RemoteDelegate", "ignore to enter full screen mode.");
         return;
       }
       VideoCallPresenter.this.onSurfaceClick();
