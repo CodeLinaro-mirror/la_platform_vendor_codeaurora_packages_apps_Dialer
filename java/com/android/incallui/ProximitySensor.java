4a9a1f0909e59425c380fb05fd8be0a60f0901b8
Xueshuai Han <xueshuai@qti.qualcomm.com>
2025-12-22 11:32:54 +0530

    Update open-source markings per LOST requirement
    Change-Id: I630924f51fed3ad8a41bb989aa93a34f80dc009e
    CRs-Fixed: 4392171

--- java/com/android/incallui/ProximitySensor.java
+++ java/com/android/incallui/ProximitySensor.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License
  *
- * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
  * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
